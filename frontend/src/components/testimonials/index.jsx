b2d76b9c0be7b0433c5e0edae8f8526f0107ba05
Ashin <ashin.jnv@gmail.com>
2023-07-02 22:06:48 +0530

    ada

--- frontend/src/components/testimonials/index.jsx
+++ frontend/src/components/testimonials/index.jsx
@@ -17,7 +17,7 @@ export default function Testimonials() {
           Web Developer
         </h6>
         <p class="mb-4">
-        I had undergone major surgery and was in need of post-surgical rehabilitation. The physio clinic in Kollam provided me with comprehensive care and guidance throughout my recovery journey. The therapists were not only skilled but also compassionate, creating a positive and motivating environment. Thanks to their efforts, I made a smooth recovery and regained my strength."
+        I had undergone major surgery and was in need of post-surgical rehabilitation. The physio clinic in Kollam provided me with comprehensive care and guidance throughout my recovery journey. The therapists were not only skilled but also compassionate, creating a positive and motivating environment. Thanks to their efforts, I made a smooth recovery and regained my strength.
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" class="inline-block w-6">
             <path fill="currentColor"
               d="M580 556h160V396H580v160Zm-360 0h160V396H220v160Zm406 220 80-160H520V336h280v288l-76 152h-98Zm-360 0 80-160H160V336h280v288l-76 152h-98Zm34-300Zm360 0Z" />
@@ -66,7 +66,7 @@ export default function Testimonials() {
           Graphic Designer
         </h6>
         <p class="mb-4">
-        "Living with chronic joint pain was affecting my daily life and overall well-being. But after visiting the physiotherapy clinic in Kollam, I found relief and hope. The knowledgeable therapists provided me with effective treatment options and taught me exercises to manage my condition. I am now able to enjoy an active lifestyle with reduced pain. Thank you for transforming my life!"
+        Living with chronic joint pain was affecting my daily life and overall well-being. But after visiting the physiotherapy clinic in Kollam, I found relief and hope. The knowledgeable therapists provided me with effective treatment options and taught me exercises to manage my condition. I am now able to enjoy an active lifestyle with reduced pain. Thank you for transforming my life!
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" class="inline-block w-6">
             <path fill="currentColor"
               d="M580 556h160V396H580v160Zm-360 0h160V396H220v160Zm406 220 80-160H520V336h280v288l-76 152h-98Zm-360 0 80-160H160V336h280v288l-76 152h-98Zm34-300Zm360 0Z" />
@@ -115,7 +115,7 @@ export default function Testimonials() {
           Marketing Specialist
         </h6>
         <p class="mb-4">
-        "I am extremely grateful to the physiotherapy clinic in Kollam for their exceptional services. As an athlete, I had faced multiple sports injuries that hindered my performance. The dedicated team of therapists provided me with tailored rehabilitation programs and guided me through the recovery process. Thanks to their expertise and support, I am now back in the game stronger than ever."
+        I am extremely grateful to the physiotherapy clinic in Kollam for their exceptional services. As an athlete, I had faced multiple sports injuries that hindered my performance. The dedicated team of therapists provided me with tailored rehabilitation programs and guided me through the recovery process. Thanks to their expertise and support, I am now back in the game stronger than ever.
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960" class="inline-block w-6">
             <path fill="currentColor"
               d="M580 556h160V396H580v160Zm-360 0h160V396H220v160Zm406 220 80-160H520V336h280v288l-76 152h-98Zm-360 0 80-160H160V336h280v288l-76 152h-98Zm34-300Zm360 0Z" />
