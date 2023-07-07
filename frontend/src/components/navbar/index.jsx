1795e607c45a280f7b0da7708ebdf39375d4653b
Ashin <ashin.jnv@gmail.com>
2023-07-07 13:43:38 +0530

    mobile

--- frontend/src/components/navbar/index.jsx
+++ frontend/src/components/navbar/index.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import styles from "./navbar.module.css";
-import Logo from '../../../public/AP-Logo.png'
+import Logo from "../../../public/AP-Logo.png";
 const links = [
   {
     id: 1,
@@ -25,35 +27,65 @@ const links = [
 ];
 
 export default function Navbar() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
- 
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
 
+  return (
+    <div className="container py-3">
+      <div
+        className={`d-flex justify-between items-center pt-5 ${styles.navbar}`}
+      >
+        <Link href="/" className={styles.logo}>
+          <Image src={Logo} alt="Logo" width={128} height={32} />
+        </Link>
 
-  return (          
-  <div className='container py-3'>
+        <button
+          className={`${styles.mobileMenuButton} ${
+            isMobileMenuOpen ? styles.open : ""
+          }`}
+          onClick={toggleMobileMenu}
+        >
+          <div class="space-y-2">
+            <span class="block w-8 h-0.5 bg-gray-600"></span>
+            <span class="block w-8 h-0.5 bg-gray-600"></span>
+            <span class="block w-5 h-0.5 bg-gray-600"></span>
+          </div>
+        </button>
 
-    <div className=" d-flex justify-between items-center pt-5">
-      <Link href="/" className={styles.logo}>
-        <Image
-        src={Logo}
-        alt="Logo"
-        width={128}
-        height={32}
-        />
-      </Link>
-
-      <div className={styles.links}>
-        {links.map((link) => (
-          <Link key={link.id} href={link.sectionId} scroll={false}  className={styles.link}>
-            {link.title}
-          </Link>
-        ))}
-
-        {/* <button className={styles.logout} >
-          Login
-        </button> */}
+        <div
+          className={`${styles.links} ${isMobileMenuOpen ? styles.open : ""}`}
+        >
+          {links.map((link) => (
+            <Link
+              key={link.id}
+              href={link.sectionId}
+              scroll={false}
+              className={styles.link}
+            >
+              {link.title}
+            </Link>
+          ))}
+        </div>
       </div>
-    </div>
+
+      {isMobileMenuOpen && (
+        <div className={styles.mobileMenu}>
+          {links.map((link) => (
+            <Link
+              key={link.id}
+              href={link.sectionId}
+              scroll={false}
+              className={styles.mobileLink}
+              onClick={toggleMobileMenu}
+              >
+              {link.title}
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
